4392279a362262c2276793f6919e5a34d91d7af2
Spiridonov Dmitry <sydd2005@gmail.com>
2020-04-30 01:37:58 +0300

    добавляет скрипт

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function getQueryParams (queryString) {
+  if (!queryString) {
+    return {};
+  }
+
+  return (/^[?#]/.test(queryString) ? queryString.slice(1) : queryString)
+    .split(`&`)
+    .reduce((params, param) => {
+      let [key, value] = param.split(`=`);
+      value = value ? decodeURIComponent(value.replace(/\+/g, ` `)) : ``;
+      params[key] = params.hasOwnProperty(key) ? [value].concat(params[key]) : value;
+      return params;
+    }, {});
+}
+
+console.log(getQueryParams(location.hash));
